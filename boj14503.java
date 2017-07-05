056c476c6c0ef4f295f74736675154f94c93efab
Dahye Kim <dahyek24@gmail.com>
2017-07-05 14:29:07 +0900

    Update boj14503.java
    예시는 맞았으나 에러 (이유는 찾는 중)

--- boj14503.java
+++ boj14503.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class boj14503 {
 	static int R[] = { -1, 0, 1, 0 };
-	static int C[] = { 0, 1, 0, -1 };
+	static int C[] = { 0, -1, 0, 1 };
 
 	public static void main(String[] args) {
-		int n, m, r, c, d, matrix[][];
-		boolean visit[][], notExplore = false;
+		int n, m, r, c, d, matrix[][], sum = 1;
+		boolean visit[][], explore = false;
 		Scanner s = new Scanner(System.in);
 		n = s.nextInt();
 		m = s.nextInt();
@@ -24,27 +24,38 @@ public class boj14503 {
 				matrix[i][j] = s.nextInt();
 
 		while (true) {
-			visit[r][c] = true;// 현재 위치를 청소한다.
-			notExplore = true; // 네 방향 모두 청소가 되어 있나?
-			System.out.println("r : " + r + "c :" + c);
-			for (int i = 0; i < 4; i++) { // 4방향을 탐색한다.
-				int nextd = (d + 1) % 4; // 현재 방향을 기준으로 왼쪽 방향을 받는다.
+			if(!visit[r][c]){
+				visit[r][c] = true;// 현재 위치를 청소한다.
+			}
+			explore = false; // 청소 가능합니까?
+			for (int i = 1; i < 5; i++) { // 4방향을 탐색한다.
+				int nextd = (d + i) % 4; // 현재 방향을 기준으로 왼쪽 방향을 받는다.
 				int nextr = r + R[nextd], nextc = c + C[nextd];
-				//배열의 인덱스가 넘어가는 것을 방지하는 코드 넣기
+				// 배열의 인덱스가 넘어가는 것을 방지하는 코드 넣기
 				if (matrix[nextr][nextc] == 0 // 왼쪽 방향이 만약 청소되지 않았다면
 						&& !visit[nextr][nextc]) { // 그리고 방문한 적이 없다면
 					d = nextd;
-					r = r + nextr;
-					c = c + nextc; // 현재 방향을 왼쪽 방향으로 바꾸고 로봇 위치 변경
-					notExplore = false;
+					r = nextr;
+					c = nextc; // 현재 방향을 왼쪽 방향으로 바꾸고 로봇 위치 변경
+					sum++;
+					explore = true;
 					break;
+				}else{
+					d = nextd;
 				}
 			}
-			if (notExplore) { // 네 방향 모두 청소가 되어있는 경우
-				//로봇의 위치를 한 칸 후진함
+
+			if (!explore) { // 네 방향 모두 청소가 되어있는 경우
+				int nextd = (d + 2) % 4; //
+				if (matrix[r + R[nextd]][c + C[nextd]] == 1)// 만약 네 방향 모두 청소가 되어
+															// 있고, 뒤가 벽이라면
+					break;// 반복문을 빠져 나간다.
+				else {
+					r = r + R[nextd];
+					c = c + C[nextd];// 아니라면 로봇의 위치를 한 칸 후진함
+				}
 			}
-			//만약 네 방향 모두 청소가 되어 있고, 뒤가 벽이라면
-			//반복문을 빠져 나간다.
 		}
+		System.out.println(sum);
 	}
 }
