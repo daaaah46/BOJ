125c20f507995531df7c2809cb45726384fe8cda
Dahye Kim <dahyek24@gmail.com>
2017-08-02 03:08:52 +0900

    Create 9498.cpp

---
+++ 9498.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	switch (n / 10) {
+	case 10:
+	case 9:
+		cout << 'A' << endl;
+		break;
+	case 8:
+		cout << 'B' << endl;
+		break;
+	case 7:
+		cout << 'C' << endl;
+		break;
+	case 6:
+		cout << 'D' << endl;
+		break;
+	default:
+		cout << 'F' << endl;
+		break;
+	}
+}
