504be615abc8626d4e39b024eff0e8b1b17058ca
Dahye Kim <dahyek24@gmail.com>
2017-08-02 03:23:49 +0900

    Update 10871.cpp
    굳이 배열 쓸 필요 없어서 다시 고침
    근데 for문을 하나 줄였는데, 시간이 더 늘어남. 왜일까?

--- 10871.cpp
+++ 10871.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int a[10001],n,x;
+	int a, n, x;
 	cin >> n >> x;
-	for (int i = 0; i < n; i++) cin >> a[i];
 	for (int i = 0; i < n; i++) {
-		if (a[i] < x) cout << a[i] << ' ';
+		cin >> a;
+		if (a < x) cout << a << ' ';
 	}
-	cout << endl;
 }
