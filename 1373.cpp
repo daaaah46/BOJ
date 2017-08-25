bcb432746232f72d6f0385a2d92e3cdf6b2d078b
Dahye Kim <dahyek24@gmail.com>
2017-08-25 13:07:02 +0900

    Update 1373.cpp

--- 1373.cpp
+++ 1373.cpp
@@ -2,17 +2,13 @@
 #include<string>
 using namespace std;
 int main() {
-	string n,v;
+	string n;
 	cin >> n;
-	if (n.size()%3 == 1) {
-		n = "00" + n;
-	}
-	else if (n.size()%3 == 2) {
+	while (n.size() % 3) {
 		n = "0" + n;
 	}
 	for (int i = 0; i<n.size(); i += 3) {
-		int a = (((n[i] - '0') * 4) + ((n[i + 1] - '0') * 2) + ((n[i + 2] - '0') * 1));
-		cout << a;
+		cout <<(((n[i] - '0') * 4) + ((n[i + 1] - '0') * 2) + ((n[i + 2] - '0') * 1));
 	}
 	cout << endl;
 }
