267147879650b5cbe41a30fa648774349e4d3be0
Dahye,KIM <halododa@gmail.com>
2018-09-25 14:23:41 +0900

    Update 4963.cpp

--- 4963.cpp
+++ 4963.cpp
@@ -1,62 +1,58 @@
 #include <iostream>
-#include <string.h>
 #include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 int map[51][51];
-bool check[51][51] = { false };
-int x_direct[] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-int y_direct[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-int ans[50] = { 0 };
-
-void bfs(int i, int j, int w, int h) {
-	queue<pair<int, int>> q;
-	check[i][j] = true;
-	q.push(make_pair(i, j));
-
-	while (!q.empty()) {
-		pair<int, int> v = q.front();
-		int x = v.first;
-		int y = v.second;
-		for (int i = 0; i < 8; i++) {
-			int xd = x + x_direct[i];
-			int yd = y + y_direct[i];
-			if ((xd > -1 && xd < h) && (yd > -1 && yd < w)) {
-				if (map[xd][yd] == 1 && check[xd][yd] == false) {
-					check[xd][yd] = true;
-					q.push(make_pair(xd, yd));
-				}
-			}
-		}
-		q.pop();
-	}
+bool visit[51][51];
+int xd[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+int yd[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int w, h;
+vector<int> v;
+void bfs(int i, int j){
+    visit[i][j] = true;
+    queue<pair<int, int>> q;
+    q.push(make_pair(i, j));
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int i = 0; i < 8; i++){
+            int xrot = x + xd[i];
+            int yrot = y + yd[i];
+            if(xrot >= 0 && xrot < h && yrot >= 0 && yrot < w){
+                if(map[xrot][yrot] == 1 && !visit[xrot][yrot]){
+                    visit[xrot][yrot] = true;
+                    q.push(make_pair(xrot, yrot));
+                }
+            }
+        }
+    }
 }
 
-int main() {
-	int w = -1, h = -1, m, sum=0, i=0;
-	while (true) {
-		cin >> w >> h;
-		
-		if (w == 0 && h == 0) break; //반복문 빠져나갈 조건
-
-		memset(map, 0, sizeof(map));
-		memset(check, false, sizeof(check));
-		sum = 0;
-		
-		for (int i = 0; i < h; i++){
-			for (int j = 0; j < w; j++) {
-				cin >> m;
-				map[i][j] = m;
-			}
-		}
-
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if (map[i][j] == 1 && check[i][j] == false) {
-					bfs(i, j, w, h);
-					sum++;
-				}
-			}
-		}
-		cout << sum << endl;
-	}
+int main(){
+    while(true){
+        scanf("%d %d", &w, &h);
+        
+        if(w == 0 && h == 0)
+            break;
+        
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                scanf("%d", &map[i][j]);
+                visit[i][j] = false;
+            }
+        }
+        
+        int g = 0;
+        for(int i = 0; i < h; i++){
+            for(int j = 0; j < w; j++){
+                if(map[i][j] == 1 && visit[i][j] == false){
+                    bfs(i, j);
+                    g++;
+                }
+            }
+        }
+        cout << g << endl;
+    }
 }
