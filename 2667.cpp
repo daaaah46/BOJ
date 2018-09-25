4f4d966727ef783d526785c76eb7fa44a4ac1217
Dahye,KIM <halododa@gmail.com>
2018-09-25 14:08:03 +0900

    Update 2667.cpp

--- 2667.cpp
+++ 2667.cpp
@@ -1,59 +1,58 @@
-#include <stdio.h>
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int map[25][25]; // 지도
-bool check[25][25] = { false };  // 방문한 것을 담을 배열 1 : 방문, 0 : 방문X
-int x_direct[4] = {0,-1,0,1};
-int y_direct[4] = { -1,0,1,0};
-int g_num = 0; //단지 갯수
+int map[26][26];
+int visit[26][26];
+int xd[4] = {0, 0, 1, -1};
+int yd[4] = {1, -1, 0, 0};
 int n;
 vector<int> v;
-void bfs(int i, int j) {
-	int h_num = 1; // 집 갯수
-	queue<pair<int,int>> q;
-	q.push(make_pair(i, j));
-	check[i][j] = true;
-	while (!q.empty()) {
-		pair<int, int> v = q.front();
-		//cout << v.first << ',' << v.second << endl;
-		int x = v.first;
-		int y = v.second;
-		for (int i = 0; i < 4; i++) {
-			int xd = x + x_direct[i];
-			int yd = y+ y_direct[i];
-			if((xd < n && xd >-1) && yd <n && yd > -1)
-				if (map[xd][yd] == 1 && check[xd][yd] == false) {
-					q.push(make_pair(xd, yd));
-					check[xd][yd] = true;
-					h_num++;
-				}
-		}
-		q.pop();
-	}
-	v.push_back(h_num);
+void bfs(int i, int j, int g){
+    int gnum = 1;
+    visit[i][j] = g;
+    queue<pair<int, int>> q;
+    q.push(make_pair(i, j));
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int i = 0; i < 4; i++){
+            int xrot = x + xd[i];
+            int yrot = y + yd[i];
+            if(xrot >= 0 && xrot < n && yrot >= 0 && yrot < n){
+                if(map[xrot][yrot] == 1 && !visit[xrot][yrot]){
+                    visit[xrot][yrot] = g;
+                    q.push(make_pair(xrot, yrot));
+                    gnum++;
+                }
+            }
+        }
+    }
+    
+    v.push_back(gnum);
 }
-int main() {
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			scanf("%1d", &map[i][j]);
-	}
 
-	for (int i = 0; i < n; i++){
-		for (int j = 0; j < n; j++) {
-			if (map[i][j] == 1 && check[i][j] == false) {
-				bfs(i, j);
-				g_num++;
-			}
-		}
-	}
-	
-	cout << g_num << endl;
-	sort(v.begin(), v.end());
-	for (int i = 0; i < v.size(); i++)
-	{
-		cout << v[i] << endl;
-	}
+int main(){
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            scanf("%1d", &map[i][j]);
+        }
+    }
+    
+    int g = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(map[i][j] == 1 && visit[i][j] == 0){
+                bfs(i, j, ++g);
+            }
+        }
+    }
+    cout << g << endl;
+    sort(v.begin(), v.end());
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << endl;
+    }
 }
