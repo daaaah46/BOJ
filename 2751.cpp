569f1d1f69c234e646d9b97573329afdd1025df1
Dahye,KIM <halododa@gmail.com>
2018-09-27 15:52:58 +0900

    Update 2751.cpp

--- 2751.cpp
+++ 2751.cpp
@@ -11,3 +11,27 @@ int main() {
 	for (int i = 0; i < n; i++)
 		printf("%d\n", m[i]);
 }
+
+//////////////////////////////////////////
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<int> a;
+    for (int i = 0; i < n; i++){
+        int t;
+        scanf("%d", &t);
+        a.push_back(t);
+    }
+    sort(a.begin(),a.end());
+    for (int i = 0; i < n; i++){
+        printf("%d\n", a.at(i));
+    }
+}
+
+	
