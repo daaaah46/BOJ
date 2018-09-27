c09e841fd5ab6928f75786feca8017d256a90142
Dahye,KIM <halododa@gmail.com>
2018-09-27 16:10:12 +0900

    Update 11650.cpp

--- 11650.cpp
+++ 11650.cpp
@@ -14,3 +14,35 @@ int main() {
 		printf("%d %d\n", v[i].first, v[i].second);
 	}
 }
+
+
+//////////////////////////////////////////////////////////////
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct Point{
+    int x, y;
+};
+bool cmp(const Point &a, const Point &b){
+    if(a.x < b.x) return true;
+    else if (a.x == b.x){
+        if(a.y < b.y){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    vector<Point> a(n);
+    for(int i = 0; i < n; i++){
+        scanf("%d %d", &a[i].x, &a[i].y);
+    }
+    sort(a.begin(), a.end(), cmp);
+    for(int i = 0; i < n; i++){
+        printf("%d %d\n", a[i].x, a[i].y);
+    }
+}
